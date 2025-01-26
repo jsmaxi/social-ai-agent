43f53cdc07a4b676a15739c1a9d50193a120732b
Jon <jsmaxi666@gmail.com>
2025-01-26 13:38:55 +0200

    Add dependencies, send posts, implement oauth1, add readme instructions

--- README.md
+++ README.md
@@ -1,9 +1,61 @@
 # Social AI Agent
 
-Written with Rust
+AI Agent bot that generates text messages and posts them on social media as configured.
+
+Written with Rust.
+
+---
+
+## Commands
+
+Created:
 
 ```console
 cargo new twitter_bot
+```
+
+Run:
+
+```console
 cd twitter_bot
+cargo build
 cargo run
 ```
+
+---
+
+## Dependencies
+
+- twitter-v2 https://crates.io/crates/twitter-v2
+
+- tokio https://crates.io/crates/tokio
+
+- cron https://crates.io/crates/cron
+
+- chrono https://crates.io/crates/chrono
+
+---
+
+## X Access
+
+Visit and sign up for free on X Developer Platform to obtain required access tokens and secrets:
+
+https://developer.x.com/
+
+X free plan currently offers:
+
+- Get limited access to X's v2 API
+- 1 environment
+- Retrieve up to 100 Posts and 500 writes per month
+- 17 requests / 24 hours PER USER
+- 17 requests / 24 hours PER APP
+
+This bot currently supports OAuth 1.0 protocol.
+
+Generate new api token with secret, and access token with secret on the platform:
+
+[Access](./images/twitter_access.png)
+
+Copy-paste those values into the bot's configuration.
+
+---
--- twitter_bot/src/main.rs
+++ twitter_bot/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use twitter_v2::{authorization::Oauth1aToken, TwitterApi};
+
+fn content() -> String {
+    let tweet_content: String = format!(
+        "Hello! This is my daily post at {}. Expect more 🫡",
+        Local::now().format("%Y-%m-%d %H:%M:%S")
+    );
+
+    println!("Text: {:?}", tweet_content);
+
+    tweet_content
+}
+
+fn oauth() -> Oauth1aToken {
+    /* Bot uses OAuth 1.0 Visit: https://developer.x.com */
+
+    let api_key: &str = "l0QchTLJdLtfXB728nlhIqsnI";
+    let api_key_secret: &str = "ubx3q6st1azIxcRqTNkm3Z8XUtqL85TUbzuD5QhUQhjm2RUdLa";
+    let access_token: &str = "1583553210521980928-zTFPjxfrCcnCp6NGwnTGvurZ53wyeE";
+    let access_token_secret: &str = "PAAr3xVr6ISKezyEX81QWEA72FykdVzz3evLUjPXfUFYc";
+
+    let token: Oauth1aToken = Oauth1aToken::new(
+        &api_key,
+        &api_key_secret,
+        &access_token,
+        &access_token_secret,
+    );
+
+    token
+}
+
+async fn tweet() -> Result<(), Box<dyn std::error::Error>> {
+    let response = TwitterApi::new(oauth())
+        .post_tweet()
+        .text(content())
+        .send()
+        .await;
+
+    match response {
+        Ok(_) => println!("Tweet posted successfully!"),
+        Err(e) => println!("Response: {:?}", e),
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tweet().await
 }
